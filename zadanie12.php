85969d97f9bc3937ba8decf7fa3716f09ded44ca
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 20:36:32 +0000

    zadanie 12 php

---
+++ zadanie12.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$day = 2;
+		switch ($day) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			echo 'Это рабочий день';
+		    break;
+			case 6:
+			case 7:
+			echo 'Это выходной день';
+		    break;
+			default:
+			echo 'Неизвестный день';
+			
+		}
+		
+        ?>
+    </body>
+</html>
