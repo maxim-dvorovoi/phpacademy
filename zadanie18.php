59b8c0f7a6af0a450c20ec6c4200a77e92de31d6
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 21:09:27 +0000

    zadanie 18 php

---
+++ zadanie18.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$a = '78';
+		$b = 78;
+		if ($a === $b) {
+			echo 'эквивалентны';
+		} else {
+			echo 'не эквивалентны';
+		}
+		
+
+        ?>
+    </body>
+</html>
