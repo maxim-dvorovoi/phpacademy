3f180fa917f79b1a65120b2e4266b4f43ddbd24d
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 19:25:04 +0000

    zadanie 1php

---
+++ zadanie1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$name = 'Andrei';
+		echo $name;
+        ?>
+    </body>
+</html>
