8f433e62e43ad2795fecc24a6542105bcd479716
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 20:25:10 +0000

    zadanie 10 php

---
+++ zadanie10.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$day = 2;
+		switch ($day) {
+			case 1:
+			echo 'Это рабочий день';
+			break;
+			case 2:
+			echo 'Это рабочий день';
+			break;
+			case 3:
+			echo 'Это рабочий день';
+			break;
+			case 4:
+			echo 'Это рабочий день';
+			break;
+			case 5:
+			echo 'Это рабочий день';
+		    break;
+		}
+		
+        ?>
+    </body>
+</html>
