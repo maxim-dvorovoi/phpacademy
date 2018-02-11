2bc6597fa3b1490ec8044077e93420075ec4929d
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 20:12:14 +0000

    zadanie9

---
+++ zadanie9.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$day = 2;
+		echo $day;
+        ?>
+    </body>
+</html>
