b6557efe0805678af23d35df605baf86d4ed3636
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 19:28:56 +0000

    zadanie 2 php

---
+++ zadanie2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$age = 21;
+		echo $age;
+        ?>
+    </body>
+</html>
