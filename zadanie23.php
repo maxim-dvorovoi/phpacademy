cd2b1dcb7d716c7a03d0ea2704f87b643324fc95
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 21:28:21 +0000

    zadanie 23 php

---
+++ zadanie23.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		echo 'privet';
+		print 'kyky';
+		printf ('4okak');
+        ?>
+    </body>
+</html>
