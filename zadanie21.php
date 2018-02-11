a7f279462cddb8d5fe7b03549ea44790a9053f9b
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 21:20:11 +0000

    zadanie 21 php

---
+++ zadanie21.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		var_dump((bool)0);
+        ?>
+    </body>
+</html>
