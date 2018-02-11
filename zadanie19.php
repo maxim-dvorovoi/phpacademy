1849db3670ec89b18460187ef4947e7c1668c3b4
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 21:14:28 +0000

    zadanie 19 php

---
+++ zadanie19.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$a = '78';
+		$b = 78;
+		var_dump ($a,$b);
+        ?>
+    </body>
+</html>
