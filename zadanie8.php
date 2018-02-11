86104bd53ea039ef41ca984f40a2429364dfdcc4
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 20:09:50 +0000

    zadanie 8 php

---
+++ zadanie8.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$age = 21;
+		
+		if ($age>17 & $age < 60){
+			echo 'Вам еще работать и работать';
+		} elseif ($age > 59) {
+		    echo 'Вам пора на пенсию';
+		} elseif ($age>=0 & $age<=17) {
+			echo 'Вам еще рано работать';
+		} else {
+			echo 'Неизвестный возраст' ;
+		}
+			
+        ?>
+    </body>
+</html>
