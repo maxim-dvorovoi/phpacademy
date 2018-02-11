c2ab7cdc068934701cfce09519c8f285cdacd8fc
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 20:45:43 +0000

    zadanie 13 php

---
+++ zadanie13.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$s = 180;
+		$t = 1;
+		$v1 = $s/$t;
+		$v2 = $s/$t*3.6;
+		echo 'Скорость в км\ч = ';
+		echo $v1;
+		echo '    ';
+		echo 'Скорость в м\c =  ';
+		echo $v2;
+		
+        ?>
+    </body>
+</html>
