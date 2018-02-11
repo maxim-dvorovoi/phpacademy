ad79395e9a44e532f8e00c3100f12a921432c8c0
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 19:41:01 +0000

    zadanie 5 php

---
+++ zadanie5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$age = 21;
+		if ($age>17 & $age<60){
+			echo 'Вам еще работать и работать';
+		}
+        ?>
+    </body>
+</html>
