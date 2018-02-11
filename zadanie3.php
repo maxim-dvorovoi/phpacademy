73895e9bf6b7e2e0dbce3cdfa8cfd13a6fa278b0
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 19:32:08 +0000

    zadanie 3 php

---
+++ zadanie3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		echo 'Меня зовут Максим';
+        ?>
+    </body>
+</html>
