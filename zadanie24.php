eadf3a8cbbd0d1b1dd1a7890dad00ea1d6772654
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 21:32:33 +0000

    zadanie 24 php

---
+++ zadanie24.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		/*6o/\bWoi KoMeHT*/
+		#avot ono kak;
+		//EWE KoMeHT
+        ?>
+    </body>
+</html>
