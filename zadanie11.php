dda8695d53b563c249a81c04a4fe337068cdb67a
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 20:28:14 +0000

    zadanie 11 php

---
+++ zadanie11.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$day = 6;
+		switch ($day) {
+			case 1:
+			echo 'Это рабочий день';
+			break;
+			case 2:
+			echo 'Это рабочий день';
+			break;
+			case 3:
+			echo 'Это рабочий день';
+			break;
+			case 4:
+			echo 'Это рабочий день';
+			break;
+			case 5:
+			echo 'Это рабочий день';
+		    break;
+			case 6:
+			echo 'Это выходной день';
+		    break;
+			case 7:
+			echo 'Это выходной день';
+		    break;
+		}
+		
+        ?>
+    </body>
+</html>
