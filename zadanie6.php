becf9188538a97682b859c94e7bcda64f58c13e7
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 19:46:39 +0000

    zadanie 6 php

---
+++ zadanie6.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$age = 21;
+		if ($age>17 & $age < 60){
+			echo 'Вам еще работать и работать';
+		} elseif ($age > 59) {
+		    echo 'Вам пора на пенсию';
+		}
+        ?>
+    </body>
+</html>
