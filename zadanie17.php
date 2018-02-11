339c3a01ec977efe2710fda48612b4576b00307a
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 21:05:45 +0000

    zadanie 17 php

---
+++ zadanie17.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$a = '78';
+		$b = 78;
+		if ($a == $b) {
+			echo 'равны';
+		} else {
+			echo 'не равны';
+		}
+		
+
+        ?>
+    </body>
+</html>
