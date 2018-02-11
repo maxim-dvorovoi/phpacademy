0794390bf8e9f77cc7146b657102c725ab53b6a5
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 20:49:48 +0000

    zadanie 14 php

---
+++ zadanie14.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$foo = 'bar';
+		$bar = 10;
+		$foo = $bar;
+		
+		
+		echo $foo;
+		
+        ?>
+    </body>
+</html>
