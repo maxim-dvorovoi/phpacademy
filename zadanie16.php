55fe428f34747e26ede7540ddec526d7c3419144
vmolo4nik <vmolo4nik@gmail.com>
2018-02-11 21:02:00 +0000

    zadanie 16 php

---
+++ zadanie16.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+        <title>Domashka</title>
+		
+    </head>
+    <body>
+        <?php
+		$a = 4;
+		$b = 4;
+		if ($a >= $b) {
+		    echo 'a =  ';
+			echo $a;
+		} else {
+			echo 'b = ';
+			echo $b;
+		}
+
+        ?>
+    </body>
+</html>
