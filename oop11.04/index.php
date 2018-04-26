0d40f8f350e77335ea5f4fb80a96a6e58cfa3077
Shkodenko V. Taras <podlom@users.noreply.github.com>
2018-04-26 23:11:51 +0300

    Update index.php
    Мне кажется лучше не кодировать полный путь "жестко"

--- oop11.04/index.php
+++ oop11.04/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "C:/Users/PC/phpstormProjects/php04.11/vendor/autoload.php";
+require_once "vendor/autoload.php";
 
 $mpdf = new \Mpdf\Mpdf();
 $mpdf->WriteHTML('<h1>Тест</h1>');
